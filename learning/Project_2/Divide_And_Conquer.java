fea616298c577ed8840e4caa3f3b105383451089
lux563624348 <lux@gwu.edu>
2018-02-13 00:45:30 -0500

    Project2_02_13

--- learning/Project_2/Divide_And_Conquer.java
+++ learning/Project_2/Divide_And_Conquer.java
@@ -25,22 +25,44 @@ import java.io.*;
 public class Divide_And_Conquer {
 	
 	static class Particles {
-		public double x=0,y=0,z=0,velocity=0;
-		//Constructor
+		public double [] x={0,0,0};
+		public double velocity=0;
 	Particles(){
 		//System.out.println("Default Constructor?");
 		}
-		
-	Particles(double x, double y, double z){
+	Particles(double... x){
 		//this(); // Invoking the default constructor, Not necessary.
-		this.x=x;this.y=y;this.z=z;
+		this.x=x;
 		}
 		
+	//Methods
 	public void Show_Status(){
-		System.out.println("Particle Coordinates: x:"+x+"  y:"+y+"  z:"+z);
-		System.out.println("Particle Velocity is:"+velocity) ;
+		System.out.println("Particle Coordinates: x: "+x[0]+" y: "+x[1]+ " z: "+x[2]);
+		System.out.println("Particle Velocity is: "+velocity) ;
 		}
+	
+		/*
+	public void Read_Data(){
+		InputStream f=new FileInputStream("input.bed");
+		
+		}
+	*/
+	public void Save_Data(){
+		try {
+			FileWriter os = new FileWriter("output.txt");
+			os.write("Coordinates: \n"); 
+			os.write(java.util.Arrays.toString(x).replace("[","").replace(",","").replace("]",""));
+			os.close();
+		} catch(IOException e){
+			System.out.print("Exception");
+			}
+		}
+	
+	
 	}
+	
+
+		
 	public static double minFunction(double n1, double n2){
 		double min;
 		if (n1>n2)
@@ -55,9 +77,10 @@ public class Divide_And_Conquer {
 		//System.out.println("Min value is: "+minFunction(1.1, 2.2));
 		Particles p1=new Particles(1.1, 2.2, 3.3);
 		Particles p2=new Particles();
-		//System.out.println(p1.x+" "+p1.y+" "+p1.z);
+		
 		p1.Show_Status();
 		p2.Show_Status();
+		p1.Save_Data();
 		
 	}
 }
